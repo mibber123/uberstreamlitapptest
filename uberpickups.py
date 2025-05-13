0f674381164316f8d42638c6e27483f47eb7b42d
ben <97813842+mibber123@users.noreply.github.com>
2025-05-13 20:42:59 +0100

    Update uberpickups.py

--- uberpickups.py
+++ uberpickups.py
@@ -6,9 +6,6 @@ st.title('streamlit uber test')
 
 DATE_COLUMN = 'date/time'
 
-
-
-
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
