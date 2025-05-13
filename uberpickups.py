5e02fe225cf990cf8f11bb62e19acd24539a8717
ben <97813842+mibber123@users.noreply.github.com>
2025-05-13 18:53:01 +0100

    Update uberpickups.py

--- uberpickups.py
+++ uberpickups.py
@@ -3,3 +3,14 @@ import pandas as pd
 import numpy as np
 
 st.title('streamlit uber test')
+
+date = 'date/time'
+
+dataurl = ('https://s3-us-west-2.amazonaws.com/' 'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def loaddata(nrows):
+    data = pd.read_csv(dataurl, nrows)
+    lowercase = lambda x: str(x).tolower()
+    data.rename(lowercase, axis = 'columns', inplace = True)
+    data[date] = pd.to_datetime(data[date])
+    return data
